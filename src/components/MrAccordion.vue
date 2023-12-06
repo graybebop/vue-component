e761f35493bac1930e7d2b84ed05b1531e9e2e98
graybebop <genial99@naver.com>
2023-12-06 22:08:34 +0900

    2023.1206 아코디언업데이트

---
+++ src/components/MrAccordion.vue
@@ -0,0 +1,43 @@
+<template>
+	<!-- <div class="accordion">
+		<div v-for="(item, index) in items" :key="index" class="accordion-item">
+			<div class="accordion-header">
+				<button class="accordion-button" type="button">Accordion Item #{{ index + 1 }}</button>
+			</div>
+			<div class="accordion-collapse collapse">
+				<div class="accordion-body">
+					<strong>This is the item #{{ index + 1 }}'s accordion body.</strong> {{ item.content }}
+				</div>
+			</div>
+		</div>
+	</div>
+  -->
+	<div class="accordion">
+		<div v-for="(item, index) in items" :key="index" class="accordion-item">
+			<div class="accordion-header">
+				<button class="accordion-button" type="button">Accordion Item #{{ index + 1 }}</button>
+			</div>
+			<div class="accordion-collapse collapse">
+				<div class="accordion-body">
+					<strong>This is the item #{{ index + 1 }}'s accordion body.</strong> {{ item.content }}
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "MrAccordion",
+	props: {
+		items: {
+			type: Array,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style scoped>
+/* 여기에 필요한 CSS를 추가하세요 */
+</style>
--- src/views/SamplePage.vue
+++ src/views/SamplePage.vue
@@ -11,9 +11,108 @@ import PostCard from "@/components/PostCard.vue";
 import MrCard from "@/components/MrCard.vue";
 </script> -->
 <template>
-  <MrCard :cardImage="'https://www.everwallpaper.co.uk/cdn/shop/products/jungle-leaves-bird-wallpaper-mural.jpg?v=1676526563'" cardTitle="Example Title" cardText="This is an example card." :cardLink="'https://example.com'" />
-  <MrCard :cardImage="'https://www.everwallpaper.co.uk/cdn/shop/products/jungle-leaves-bird-wallpaper-mural.jpg?v=1676526563'" cardTitle="Another Title" cardText="This is another example card." :cardLink="'https://example.com'" />
+	<div class="container">
+		<div class="row">
+			<div class="col]-12">
+				<MrCard
+					:cardImage="'https://www.everwallpaper.co.uk/cdn/shop/products/jungle-leaves-bird-wallpaper-mural.jpg?v=1676526563'"
+					cardTitle="Example Title"
+					cardText="This is an example card."
+					:cardLink="'https://example.com'"
+				/>
+				<MrCard
+					:cardImage="'https://www.everwallpaper.co.uk/cdn/shop/products/jungle-leaves-bird-wallpaper-mural.jpg?v=1676526563'"
+					cardTitle="Another Title"
+					cardText="This is another example card."
+					:cardLink="'https://example.com'"
+				/>
+				<MrAccordion :items="items" />
+				<div>
+					<!-- accordion item  -->
+					<div class="accordion-box">
+						<!-- accordion-btn 클릭시 활성화 되면 on <div class="accordion-items on">-->
+						<div class="accordion-items on">
+							<div class="accordion-hdr">
+								<button class="accordion-btn" type="button">Accordion Item</button>
+							</div>
+							<div class="accordion-cont">
+								<strong>This is the item accordion body.</strong>
+							</div>
+						</div>
+					</div>
+					<!--// accordion item  -->
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 <script setup>
 import MrCard from "@/components/MrCard.vue";
+import MrAccordion from "@/components/MrAccordion.vue";
+
+const items = [];
 </script>
+
+<style lang="scss">
+.accordion-box {
+	border-radius: 16px;
+	border: 1px solid #eceef0;
+	background: #fff;
+	overflow: hidden;
+	box-shadow: 0px 8px 8px 0px rgba(0, 0, 0, 0.02);
+
+	.accordion-items {
+		.accordion-hdr {
+			position: relative;
+
+			.accordion-btn {
+				width: 100%;
+				display: block;
+				padding: 20px;
+				color: #2e3033;
+
+				font-size: 16px;
+				font-style: normal;
+				font-weight: 700;
+				line-height: 100%; /* 16px */
+				text-align: left;
+				border: 0;
+				margin: 0;
+				display: flex;
+				flex-direction: row;
+				align-items: center;
+				flex-wrap: nowrap;
+				justify-content: space-between;
+				&:after {
+					content: "";
+					display: block;
+					width: 18px;
+					height: 18px;
+					background-repeat: no-repeat;
+					background-position: center;
+					background-size: 18px;
+					background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='18' height='18' viewBox='0 0 18 18' fill='none'%3E%3Cpath d='M14.8228 12L8.91133 5.9999L2.9999 12' stroke='%232E3033' stroke-width='1.3' stroke-linecap='round' stroke-linejoin='round'/%3E%3C/svg%3E");
+					transform: rotate(180deg);
+				}
+			}
+		}
+
+		.accordion-cont {
+			padding: 0 20px 20px;
+			display: none;
+		}
+
+		&.on {
+			.accordion-btn {
+				&:after {
+					transform: rotate(0);
+				}
+			}
+
+			.accordion-cont {
+				display: block;
+			}
+		}
+	}
+}
+</style>
