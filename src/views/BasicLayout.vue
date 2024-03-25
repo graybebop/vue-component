c2ea403c5d41d4e61de5219c7391f0afd28c8466
graybebop <genial99@naver.com>
2024-03-25 13:00:40 +0900

    기본레이아웃

---
+++ src/views/BasicLayout.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- app wrapper -->
+  <div class="kt-wrapper">
+    <!-- header -->
+    <div class="kt-header-wrapper">
+      <div class="app-header-menu">메뉴</div>
+      <div class="app-navbar">메뉴</div>
+    </div>
+    <!--// header -->
+    <!-- app main -->
+    <div class="kt-main">
+      <div class="kt-toolbar-container">
+        <div class="page-title">타이틀</div>
+        <div class="page-breadcrumb">페이지경로</div>
+      </div>
+    </div>
+    <!--// app main -->
+    <!-- page content -->
+    <div class="kt-content">
+      <!-- page content container -->
+      <div class="kt-content-container">
+        <!-- 페이지별 구성요소 -->
+        <div class="">구성요소</div>
+        <!--// 페이지별 구성요소 -->
+      </div>
+      <!-- page content container -->
+    </div>
+    <!--// page content -->
+    <!-- footer -->
+    <div class="kt-footer">푸터</div>
+    <!--// footer -->
+  </div>
+  <!--// app wrapper -->
+</template>
